abdd430789aaf771e830935167ab741f346f59df
Manasa-H-S <134793227+Manasa-H-S@users.noreply.github.com>
2026-05-19 14:38:21 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = `${process.env.REACT_APP_API_URL || 'http://localhost:5000/api'}/auth`;
 
 export const loginUser = async (data) => {
   const response = await fetch(`${API_URL}/login`, {
