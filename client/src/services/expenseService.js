9f82f55a9c720592b9d4263b83c5dfa4c53985a5
Manasa-H-S <manasahs7s@gmail.com>
2026-05-20 15:58:37 +0530

    separating client and server

---
+++ client/src/services/expenseService.js
@@ -0,0 +1,70 @@
+const API_URL = 'http://localhost:5000/api/expenses';
+
+const getAuthHeaders = () => {
+    const token = localStorage.getItem('token');
+
+    return {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+    };
+};
+
+export const getExpenses = async () => {
+    const response = await fetch(API_URL, {
+        headers: getAuthHeaders(),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch expenses');
+    }
+
+    return await response.json();
+};
+
+export const addExpense = async (expense) => {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: getAuthHeaders(),
+        body: JSON.stringify(expense),
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message);
+    }
+
+    return data;
+};
+
+export const updateExpense = async (expense) => {
+    const response = await fetch(
+        `${API_URL}/${expense.id}`,
+        {
+            method: 'PUT',
+            headers: getAuthHeaders(),
+            body: JSON.stringify(expense),
+        }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message);
+    }
+    return data;
+};
+
+export const deleteExpense = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: 'DELETE',
+        headers: getAuthHeaders(),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message);
+    }
+
+    return data;
+};
